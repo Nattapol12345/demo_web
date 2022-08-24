2b37032ef22585209b927d8fa6d114ec505b10d9
Nattapol <tarza9759@gmall.com>
2022-08-24 09:59:52 +0700

    Youtoub+carousel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,47 @@
 <div class="container px-4 px-lg-5">
     <!-- Heading Row-->
     <div class="row gx-4 gx-lg-5 align-items-center my-5">
-        <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg" alt="..." /></div>
+        <div class="col-lg-7">
+            <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active" data-bs-interval="10000">
+                                <img src="https://wallpaperaccess.com/full/3717842.png" width='150px' height='300px' class="d-block w-100" alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                    <h5></h5>
+                                    <p></p>
+                                    </div>
+                            </div>
+                        <div class="carousel-item" data-bs-interval="2000">
+                            <img src="https://wallpaperaccess.com/full/3717847.jpg" width='150px' height='300' class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                <h5></h5>
+                                <p></p>
+                                </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://preview.redd.it/43egns6irm981.jpg?auto=webp&s=6da199931e9151167720f9929f96d7032aeec40f" width='150' height='300' class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                <h5></h5>
+                                <p></p>
+                                </div>
+                            </div>
+                        </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+                </div>
+            </div>
+        
         <div class="col-lg-5">
             <h1 class="font-weight-light">Business Name or Tagline</h1>
             <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
@@ -14,7 +54,9 @@
     </div>
         <!-- Call to Action-->
         <div class="card text-white bg-secondary my-5 py-4 text-center">
-            <div class="card-body"><p class="text-white m-0">This call to action card is a great place to showcase some important information or display a clever tagline!</p></div>
+            <div class="card-body col-lg-7">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/quztsT8nnpY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
         </div>
         <!-- Content Row-->
         <div class="row gx-4 gx-lg-5">
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -12,7 +12,8 @@
         <!-- Core theme CSS (includes Bootstrap)-->
         {{-- <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" type="text/css" >
         <script src="{{ asset("assets/js/scripts.js") }}"></script> --}}
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
         <!-- Responsive navbar-->
