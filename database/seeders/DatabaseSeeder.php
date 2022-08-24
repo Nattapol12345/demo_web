0c91e58d9b23bc69dafdac601f25386ce04c3ef6
kornhub <davidbetagen@gmail.com>
2022-08-24 14:32:39 +0700

    add seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,33 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('posts')->insert([
-            'title' => "Pecus subseco et uxor conventus arguo provident attollo inflammatio tepidus ara est voluptates advoco recusandae tricesimus attonbitus soluta",
-            'detail' => "At tibi animadverto. Delinquo callide vix. Arbustum aspernatur acidus. Spiculum audax delinquo. Infit sol tepidus. Speciosus aliquid curso. Benevolentia ancilla texo. Est incidunt pel. Alioqui caries aperio. Nam statim absconditus. Defungo reprehenderit callide. Socius somnus ago. Talus aut bibo. Articulus ratione et. Sono careo absens. Iste corona dolores. Tristis culpo eligendi. Sodalitas textus aspernatur. Clamo ut ascisco. Valeo defaeco illum. Traho umquam uter. Beatae toties averto",
-        ]);
-        DB::table('posts')->insert([
-            'title' => "Umquam saepe tamen triginta calco combibo nam cohaero optio arguo consequatur creta",
-            'detail' => "Adhuc amiculum deorsum. Talus patria antea. Degusto voluptas saepe. Corrupti coaegresco pecus. Qui viridis comminor. Apparatus aperio venustas. Crapula validus varius. Possimus convoco enim. Comes timor vulgivagus. Asperiores commemoro demitto. Quisquam qui acervus. Vaco bestia nihil. Cruciamentum clibanus vox. Quia volup ulciscor. Solvo est spectaculum.",
-        ]);
-        DB::table('posts')->insert([
-            'title' => "Consequuntur vulnero sum tepesco defendo vacuus sursum villa eligendi vociferor explicabo debitis acquiro.",
-            'detail' => "empus turba defluo. Est sumptus angelus. Caute suus admoveo. Adhuc qui admoveo. Quibusdam testimonium universe. Cognomen vestrum audeo. Sol anser xiphias. Vultuosus conturbo absum. Clementia amiculum sui. Colligo denuo vulnero. Tempora chirographum voluptate. Campana thesaurus tamquam. Ater sto ipsam. Non magni studio. Coma adduco ea. Coniuratio coepi arbustum. Usque cena capitulus. Magnam thalassinus defessus. Suffoco tardus bibo.",
-        ]);
-        DB::table('posts')->insert([
-            'title' => "Pecus subseco et uxor conventus arguo provident attollo inflammatio tepidus ara est voluptates advoco recusandae tricesimus attonbitus soluta",
-            'detail' => "At tibi animadverto. Delinquo callide vix. Arbustum aspernatur acidus. Spiculum audax delinquo. Infit sol tepidus. Speciosus aliquid curso. Benevolentia ancilla texo. Est incidunt pel. Alioqui caries aperio. Nam statim absconditus. Defungo reprehenderit callide. Socius somnus ago. Talus aut bibo. Articulus ratione et. Sono careo absens. Iste corona dolores. Tristis culpo eligendi. Sodalitas textus aspernatur. Clamo ut ascisco. Valeo defaeco illum. Traho umquam uter. Beatae toties averto",
-        ]);
-        DB::table('posts')->insert([
-            'title' => "Pecus subseco et uxor conventus arguo provident attollo inflammatio tepidus ara est voluptates advoco recusandae tricesimus attonbitus soluta",
-            'detail' => "At tibi animadverto. Delinquo callide vix. Arbustum aspernatur acidus. Spiculum audax delinquo. Infit sol tepidus. Speciosus aliquid curso. Benevolentia ancilla texo. Est incidunt pel. Alioqui caries aperio. Nam statim absconditus. Defungo reprehenderit callide. Socius somnus ago. Talus aut bibo. Articulus ratione et. Sono careo absens. Iste corona dolores. Tristis culpo eligendi. Sodalitas textus aspernatur. Clamo ut ascisco. Valeo defaeco illum. Traho umquam uter. Beatae toties averto",
-        ]);
-        DB::table('posts')->insert([
-            'title' => "Pecus subseco et uxor conventus arguo provident attollo inflammatio tepidus ara est voluptates advoco recusandae tricesimus attonbitus soluta",
-            'detail' => "At tibi animadverto. Delinquo callide vix. Arbustum aspernatur acidus. Spiculum audax delinquo. Infit sol tepidus. Speciosus aliquid curso. Benevolentia ancilla texo. Est incidunt pel. Alioqui caries aperio. Nam statim absconditus. Defungo reprehenderit callide. Socius somnus ago. Talus aut bibo. Articulus ratione et. Sono careo absens. Iste corona dolores. Tristis culpo eligendi. Sodalitas textus aspernatur. Clamo ut ascisco. Valeo defaeco illum. Traho umquam uter. Beatae toties averto",
-        ]);
-        DB::table('posts')->insert([
-            'title' => "Pecus subseco et uxor conventus arguo provident attollo inflammatio tepidus ara est voluptates advoco recusandae tricesimus attonbitus soluta",
-            'detail' => "At tibi animadverto. Delinquo callide vix. Arbustum aspernatur acidus. Spiculum audax delinquo. Infit sol tepidus. Speciosus aliquid curso. Benevolentia ancilla texo. Est incidunt pel. Alioqui caries aperio. Nam statim absconditus. Defungo reprehenderit callide. Socius somnus ago. Talus aut bibo. Articulus ratione et. Sono careo absens. Iste corona dolores. Tristis culpo eligendi. Sodalitas textus aspernatur. Clamo ut ascisco. Valeo defaeco illum. Traho umquam uter. Beatae toties averto",
-        ]);
+       
     }
 }
