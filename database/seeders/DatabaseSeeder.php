89dc2801df9823544b9b97cd10222b5b1e7b1c04
kornhub <davidbetagen@gmail.com>
2022-08-24 14:22:38 +0700

    ädd seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('posts')->insert([
+            'title' => "Pecus subseco et uxor conventus arguo provident attollo inflammatio tepidus ara est voluptates advoco recusandae tricesimus attonbitus soluta",
+            'detail' => "At tibi animadverto. Delinquo callide vix. Arbustum aspernatur acidus. Spiculum audax delinquo. Infit sol tepidus. Speciosus aliquid curso. Benevolentia ancilla texo. Est incidunt pel. Alioqui caries aperio. Nam statim absconditus. Defungo reprehenderit callide. Socius somnus ago. Talus aut bibo. Articulus ratione et. Sono careo absens. Iste corona dolores. Tristis culpo eligendi. Sodalitas textus aspernatur. Clamo ut ascisco. Valeo defaeco illum. Traho umquam uter. Beatae toties averto",
+        ]);
+        PostSeeder::class;
     }
 }
